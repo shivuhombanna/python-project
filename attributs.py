84db88bd1e4a0010913eaa20bf853f03c2a5884c
Shivaraj H <140573544+shivuhombanna@users.noreply.github.com>
2026-07-08 22:09:03 +0530

    Add files via upload

---
+++ attributs.py
@@ -0,0 +1,10 @@
+class Cars:
+    def __init__(self,brand,module):
+        self.brand=brand
+        self.module=module
+    def cars_info(self):
+        print(f"car brand name ={self.brand},car model name is {self.module}")
+bmw=Cars("BMW","M4 CS compitaion ")
+bmw.cars_info()
+toyota=Cars("forchunar","4*4 drive")
+toyota.cars_info()
