a7141e7aee28c7b03952beeff3d157090dbd74d7
Shivaraj H <140573544+shivuhombanna@users.noreply.github.com>
2026-07-09 23:12:44 +0530

    Add files via upload

---
+++ constarctors.py
@@ -0,0 +1,13 @@
+class Human:
+    def __init__(self,name="gotilla",age=0):
+        self.name=name
+        self.age=age
+    def activity(self):
+        print(f"{self.name} plying a football")
+s=Human("shivu",77)
+s.activity()
+papu=Human()
+papu.name="shivi"
+papu.activity()
+
+        
