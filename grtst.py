6fb13a204570d1af2c3eba7867ae2cd3a445b811
Shivaraj H <140573544+shivuhombanna@users.noreply.github.com>
2026-07-16 23:38:04 +0530

    Restore and correct vehicle and animal classes

--- grtst.py
+++ grtst.py
@@ -1,33 +1,33 @@
-# class Stdent:
-#     def __init__(self,name):
-#         self.__name=name
-#     def get_name(self):
-#         return self.__name
-#     def set_name(self,name):
-#         self.__name=name
+class Stdent:
+    def __init__(self,name):
+        self.__name=name
+    def get_name(self):
+        return self.__name
+    def set_name(self,name):
+        self.__name=name
     
-# s=Stdent('shiva')
-# print(f"name:{s.get_name()}"
+s=Stdent('shiva')
+print(f"name:{s.get_name()}"
 
-#super()
+super()
 
-# class Animal:
-#     def __init__(self,name):
-#         self.name=name
-#     def sound(self):
-#         print(f"{self.name} make sound")
+class Animal:
+    def __init__(self,name):
+        self.name=name
+    def sound(self):
+        print(f"{self.name} make sound")
 
-# class Dog(Animal):
-#     def __init__(self, name,breed):
-#         super().__init__(name)
-#         self.breed=breed
+class Dog(Animal):
+    def __init__(self, name,breed):
+        super().__init__(name)
+        self.breed=breed
 
-#     def sound(self):
-#         super().sound()
-#         print(f"{self.name} barking")
+    def sound(self):
+        super().sound()
+        print(f"{self.name} barking")
 
-# dog=Dog("deva","labratri")
-# dog.sound()
+dog=Dog("deva","labratri")
+dog.sound()
 
 from abc import ABC,abstractmethod
 class Vechile(ABC):
